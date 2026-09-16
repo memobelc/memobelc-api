93de340e94509ead502524435672faa1274aae7d
Cleby Francisco <clebysilva5@gmail.com>
2026-09-16 00:25:02 -0300

    feat: remove payment controller and related route registration

--- src/app/routes/routes.py
+++ src/app/routes/routes.py
@@ -6,7 +6,6 @@ from src.app.controllers.card_controller import card_blueprint
 from src.app.controllers.user_progress_controller import user_progress_blueprint
 from src.app.controllers.videos_controller import video_blueprint
 from src.app.controllers.chat_controller import chat_blueprint
-from src.app.controllers.payment_controller import payment_blueprint
 from src.app.controllers.classroom_controller import classroom_blueprint
 from src.app.controllers.notification_controller import notification_blueprint
 from src.app.controllers.user_streak_controller import user_streak_blueprint
@@ -35,7 +34,6 @@ routes.register_blueprint(card_blueprint, url_prefix="/card")
 routes.register_blueprint(video_blueprint, url_prefix="/video")
 routes.register_blueprint(user_progress_blueprint, url_prefix="/progress")
 routes.register_blueprint(chat_blueprint, url_prefix="/chat")
-routes.register_blueprint(payment_blueprint, url_prefix="/payment")
 routes.register_blueprint(classroom_blueprint, url_prefix="/classroom")
 routes.register_blueprint(notification_blueprint, url_prefix="/notifications")
 routes.register_blueprint(user_streak_blueprint, url_prefix="/streak")
